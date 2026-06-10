ec1f18d9fb3f7e5049b04a689e7da8df4106177b
Yanxin Luo <yxluo3@Yanxins-MacBook-Pro.local>
2026-06-10 17:31:55 -0400

    thirday

--- sphere.h
+++ sphere.h
@@ -6,10 +6,12 @@
 class sphere : public hittable
 {
 public:
-    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3 &center, double radius, shared_ptr<material> mat)
+        : center(center), radius(std::fmax(0, radius)), mat(mat) {}
 
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-    // bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override {
+    bool hit(const ray &r, interval ray_t, hit_record &rec) const override
+    {
+        // bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override {
         vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
@@ -23,8 +25,9 @@ public:
 
         // Find the nearest root that lies in the acceptable range.
         auto root = (h - sqrtd) / a;
-         if (!ray_t.surrounds(root)) {
-        // if (root <= ray_tmin || ray_tmax <= root) {
+        if (!ray_t.surrounds(root))
+        {
+            // if (root <= ray_tmin || ray_tmax <= root) {
             root = (h + sqrtd) / a;
             // if (root <= ray_tmin || ray_tmax <= root)
             if (!ray_t.surrounds(root))
@@ -35,6 +38,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -42,6 +46,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
